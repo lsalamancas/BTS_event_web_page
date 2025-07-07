eeb6b21b0089aae9d208c718d5b71e278ac2b046
lsalamancas <luis.salamanca-s@mail.escuelaing.edu.co>
2025-07-07 07:42:26 -0500

    setting build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-        locales: ['en', 'it', 'es'],
-        defaultLocale: 'en',
-        
-    },
     output: 'static',
     base: '/BTS_event_web_page/'
 });
